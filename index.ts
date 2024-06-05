d01b579d61eb49cb3a70ad99ae4404422013ff17
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-06-05 16:25:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,79 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+class Student {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Person {
+  students: Student[] = [];
+
+  addStudent(obj: Student) {
+    this.students.push(obj);
+  }
+}
+const persons = new Person();
+const programStart = async (persons: Person) => {
+  do {
+    console.log(chalk.magenta("*".repeat(18)));
+
+    console.log(chalk.bgGreen.magenta("**** Welcome **** "));
+
+    console.log(chalk.magenta("*".repeat(18)));
+
+    const ans = await inquirer.prompt([
+      {
+        name: "select",
+        type: "list",
+        message: chalk.blue.bold("Please Select Option ..."),
+        choices: ["staff", "student", "exit"],
+      },
+    ]);
+    if (ans.select == "staff") {
+      console.log(
+        chalk.green.bold.italic(
+          "you approach the staff room,and feel free to ask any questions"
+        )
+      );
+    } else if (ans.select == "student") {
+      const answers = await inquirer.prompt([
+        {
+          name: "student",
+          type: "input",
+          message: chalk.magenta("Enter the student name you want to select "),
+        },
+      ]);
+      const student = persons.students.find(
+        (val) => val.name == answers.student
+      );
+      if (!student) {
+        const name = new Student(answers.student);
+        persons.addStudent(name);
+        console.log(
+          chalk.green.bold(`Hello I am ${name.name}, Nice to Meet you!`)
+        );
+        console.log(chalk.magenta.bold("Student added successfully"));
+        console.log(chalk.red.bold("Current students List"));
+        console.log(persons.students);
+      }
+      if (student) {
+        console.log(
+          chalk.green.bold.italic(
+            `Hello I am ${student.name}, I am Fine!......`
+          )
+        );
+        console.log(chalk.blue(persons.students));
+      }
+    } else if (ans.select == "exit") {
+      console.log(chalk.magenta("*".repeat(28)));
+
+      console.log(chalk.green.bgGray.blue("Thank you for visiting Us !"));
+      console.log("\n");
+      console.log(chalk.magenta("*".repeat(28)));
+    }
+  } while (true);
+};
+programStart(persons);
